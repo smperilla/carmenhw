22c858dc26ac070c15be684c7e1b38c050967ae3
sperilla <smperilla@gmail.com>
2023-10-18 20:45:42 -0400

    first clue

--- answers.sql
+++ answers.sql
@@ -24,6 +24,8 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+--SELECT name, region, population FROM country WHERE region = 'Southern Europe' ORDER BY population ASC;
+--VATICAN
 
 
 
