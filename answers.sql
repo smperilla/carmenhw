616ba87cbb37b22e691df3a2d87b05c87ae879e5
sperilla <smperilla@gmail.com>
2023-10-18 21:39:05 -0400

    finished

--- answers.sql
+++ answers.sql
@@ -60,6 +60,8 @@ SELECT name FROM city WHERE name LIKE '%San Mar%';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
+SELECT name, id FROM city WHERE population ='91084'
+--Santa Monica
 
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
