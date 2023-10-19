fd1f4b5214f4f40c9b49075f3dd51beb98e53f2a
sperilla <smperilla@gmail.com>
2023-10-18 20:51:17 -0400

    Clue Two

--- answers.sql
+++ answers.sql
@@ -30,7 +30,8 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-
+--SELECT language from countrylanguage WHERE countrycode = 'VAT';
+--Italian
 
 
 
